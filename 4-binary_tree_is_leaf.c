78e6fea30688157be0a1c919254e6bfb43823bd0
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-07 16:08:21 -0500

    4.0 - base

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, 0 otherwise
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{}
