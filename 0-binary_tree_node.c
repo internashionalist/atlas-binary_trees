fc584349b3cb25e0f692815e4799f5cac8f2ce2f
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-07 15:23:18 -0500

    0.4 - betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node; /* create a new node */
-	
+
 	new_node = malloc(sizeof(binary_tree_t)); /* malloc for new node */
 	if (!new_node) /* if malloc fails */
 		return (NULL); /* return NULL */
