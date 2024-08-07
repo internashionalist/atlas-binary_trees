db80f69ee822c1f19ec68c992bc32731b1327719
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-07 15:43:33 -0500

    1.1 - null case and malloc for new node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,8 +5,17 @@
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
  *
- * Return: pointer to the created node, or NULL on failure
+ * Return: pointer to the created node, or NULL
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+       binary_tree_t *new_node; /* create a new node */
+
+       if (!parent) /* if there is no parent */
+              return (NULL); /* return NULL */
+
+       new_node = malloc(sizeof(binary_tree_t)); /* malloc for new node */
+       if (!new_node) /* if malloc fails */
+              return (NULL); /* return NULL */
+
