a7d7bcbe184a5303dc76087978ba72c286cedf1f
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 16:04:10 -0500

    3.0 - base

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes and frees an entire binary tree
+ * @tree: pointer to the root node of the tree to delete
+ *
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
