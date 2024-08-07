ee2c5df8a0d602b16d3d28d12961ea185545d885
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 15:16:14 -0500

    0.0 - base

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node
+ * @value: value inside the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{}
