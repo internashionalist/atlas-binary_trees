4d3cf23ca9173ca61f68bfc8ce99773cff00fbc8
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 16:06:17 -0500

    3.1 - can't valgrind, so I hope this actually works

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,3 +9,10 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
+       if (tree == NULL) /* if there's no frickin tree */
+              return; /* make like a tree */
+
+       binary_tree_delete(tree->left); /* delete left child */
+       binary_tree_delete(tree->right); /* delete right child */
+       free(tree); /* free the tree */
+}
