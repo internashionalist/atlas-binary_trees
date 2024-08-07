668899a44f05f83326d57733cd9f49f5ec228c68
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 15:22:16 -0500

    0.2 - pretty trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,3 +16,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!new_node) /* if malloc fails */
 		return (NULL); /* return NULL */
 
+	new_node->n = value; /* set value of new node */
+	new_node->parent = parent; /* set parent of new node */
+	new_node->left = NULL; /* set left child of new node to NULL */
+	new_node->right = NULL; /* set right child of new node to NULL */
+
+	return (new_node); /* return pointer to new node */
+}
