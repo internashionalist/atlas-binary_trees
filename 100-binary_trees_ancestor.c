5e703260e6b0f360c41f95cfd90019cdb733cef5
internashionalist <taylor.thames@atlasschool.com>
2024-08-08 07:26:56 -0500

    100.1 - null cases I think

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,14 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * binary_trees_ancestor - finds the LCA of two nodes
  * @first: pointer to the first node
  * @second: pointer to the second node
  *
- * Return: pointer to the lowest common ancestor node, or NULL if no ancestor
+ * Return: pointer to the LCA node, or NULL if no ancestor
  * 	or if first or second are NULL
  */
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 					const binary_tree_t *second)
 {
+	if (!first || !second) /* if no first or second node */
+		return (NULL); /* no ancestor */
+
+	if (first == second) /* if first and second are same node */
+		return ((binary_tree_t *)first); /* return that node */
