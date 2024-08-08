e55a3ca965316096434dab6ba5e6f85e4dfda0fd
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 21:55:28 -0500

    6.2 - runs

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,3 +13,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func) /* if there's no tree or function */
 		return; /* make like a tree */
 
+	func(tree->n); /* call function on root */
+	binary_tree_preorder(tree->left, func); /* traverse left subtree */
+	binary_tree_preorder(tree->right, func); /* traverse right subtree */
+}
