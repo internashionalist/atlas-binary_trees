892b348b2854b7338638a81e33d97bb6c548d8de
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-07 22:49:26 -0500

    12.0 - base

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the counted tree
+ *
+ * Return: number of leaves, or 0 if tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
