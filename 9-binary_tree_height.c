c17f9ca448d9d8e760b24d31bd6bcc96981251cf
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 20:51:50 -0500

    9.0 - base

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of tree being measured
+ *
+ * Return: height of tree, or 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{}
