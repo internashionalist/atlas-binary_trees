28839ed080f31382a1fc6b96be9506b93eea3faa
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-08 05:43:48 -0500

    16.0 - base

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the potentially perfect tree
+ *
+ * Return: 1 if perfect, 0 if garbage
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
