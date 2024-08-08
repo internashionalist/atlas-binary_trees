011b1912ba8efe5243be38827237f6a562111991
internashionalist <taylor.thames@atlasschool.com>
2024-08-08 02:01:44 -0500

    13.0 - base

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the counted tree
+ *
+ * Return: number of nodes with at least 1 child, or 0 if tree is NULL
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{}
