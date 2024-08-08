e7528a57e6f93862ea287f0fd40f856df43f556d
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-07 21:57:30 -0500

    7.0 - base

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - traverses a binary tree using in-order
+ * @tree: pointer to the root node of traversed tree
+ * @func: pointer to a function to call for each node
+ *
+ * Return: void
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{}
