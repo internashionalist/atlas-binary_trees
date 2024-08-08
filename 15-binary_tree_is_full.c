2687a0edf318ba67b567685b158ad11e87cadd8b
internashionalist <taylor.thames@atlasschool.com>
2024-08-08 05:06:01 -0500

    15.0 - base

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if full, 0 otherwise
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{}
