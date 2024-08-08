5e60ac2e30d71fbf93e630e6e83ba4412358197e
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-08 01:59:50 -0500

    12.1 - this took way too long

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,3 +9,13 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves = 0; /* initialize leaf counter */
+
+	if (!tree) /* if no tree */
+		return (0); /* no leaves */
+	if (!tree->left && !tree->right) /* if no children */
+		return (1); /* that's a leaf! */
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	/* add leaves in both subtrees */
+	return (leaves); /* return total number of leaves */
+}
