2976963876863dbc23398f83fc1687bbd7a68b1b
internashionalist <taylor.thames@atlasschool.com>
2024-08-08 05:33:29 -0500

    15.3 - not matching up with example

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,11 +9,14 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-       if (!tree) /* if no tree */
-              return (0); /* def not full */
+	if (!tree) /* if no tree */
+		return (0); /* def not full */
 
-       if (!binary_tree_is_full(tree->left) || !binary_tree_is_full(tree->right))
-              return (0); /* if either subtree is not full */
+	if (!binary_tree_is_full(tree->left) || !binary_tree_is_full(tree->right))
+		return (0); /* if either subtree is not full */
 
-       
-       
+	if (!tree->left && !tree->right)
+		return (1); /* if tree IS a leaf */
+
+	return (1); /* tree is full */
+}
