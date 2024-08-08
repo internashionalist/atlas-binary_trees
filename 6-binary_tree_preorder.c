8c8d9ace06692333dc19a716883c8d117143560b
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 21:48:53 -0500

    6.0 - base

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverses a binary tree using pre-order
+ * @tree: pointer to the root node of traversed tree
+ * @func: pointer to a function to call for each node
+ *
+ * Return: void
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
