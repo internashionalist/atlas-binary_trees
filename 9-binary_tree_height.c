2daaf61bef009fce84864ec2e9e8741af189fcc9
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-07 21:45:09 -0500

    9.2 - changed null case, added root later

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,14 +12,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
        size_t Lheight; /* height of left subtree */
        size_t Rheight; /* height of right subtree */
 
-       if (tree == NULL) /* if there's no frickin tree */
+       if (!tree || (!tree->left && !tree->right)) /* if no tree or no children */
               return (0); /* make like a tree */
 
-       Lheight = 1 + binary_tree_height(tree->left); /* measure left subtree */
-       Rheight = 1 + binary_tree_height(tree->right); /* measure right subtree */
+       Lheight = binary_tree_height(tree->left); /* measure left subtree */
+       Rheight = binary_tree_height(tree->right); /* measure right subtree */
 
        if (Lheight > Rheight) /* if left subtree is taller */
-              return (Lheight); /* return height of left subtree */
+              return (Lheight + 1); /* return left subtree height plus root */
        else
-              return (Rheight); /* return height of right subtree */
+              return (Rheight + 1); /* return subtree height plus root */
 }
