d7a0107ab3e2eb5f6945603b36ded6e2c531235b
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-08 12:02:24 -0500

    100.2 - HAHAHAHAHA

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -17,3 +17,18 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	if (first == second) /* if first and second are same node */
 		return ((binary_tree_t *)first); /* return that node */
+	
+	while (first) /* traverse ancestors of first node */
+	{
+		const binary_tree_t *temp_node = second; /* set temp node */
+
+		while (temp_node) /* traverse ancestors of second node */
+		{
+			if (first == temp_node) /* if first and second are same node */
+				return ((binary_tree_t *)first); /* that's the LCA */
+			temp_node = temp_node->parent; /* move second up the tree */
+		}
+		first = first->parent; /* move first up the tree */
+	}
+	return (NULL); /* otherwise, no ancestor */
+}
