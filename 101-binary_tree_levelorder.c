f324dc9712364843b613612700f89ca9ed9d75cb
internashionalist <taylor.thames@atlasschool.com>
2024-08-08 12:20:56 -0500

    101.0 - base

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - traverses a binary tree using level-order
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
+ *
+ * Return: void
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{}
