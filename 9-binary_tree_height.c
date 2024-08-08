d40b41af7692b95bd5b2c3d75cea8256f7305d5d
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 21:40:54 -0500

    9.1 - compiles but doesn't match example

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,4 +8,18 @@
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
-{}
+{
+       size_t Lheight; /* height of left subtree */
+       size_t Rheight; /* height of right subtree */
+
+       if (tree == NULL) /* if there's no frickin tree */
+              return (0); /* make like a tree */
+
+       Lheight = 1 + binary_tree_height(tree->left); /* measure left subtree */
+       Rheight = 1 + binary_tree_height(tree->right); /* measure right subtree */
+
+       if (Lheight > Rheight) /* if left subtree is taller */
+              return (Lheight); /* return height of left subtree */
+       else
+              return (Rheight); /* return height of right subtree */
+}
