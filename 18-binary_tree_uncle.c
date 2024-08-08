617063a6cd1f313d1c7ed5bd9331a3a4e17ac5c8
internashionalist <taylor.thames@atlasschool.com>
2024-08-08 06:27:12 -0500

    18.0 - base

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node whose uncle we're finding
+ *
+ * Return: pointer to the uncle node, or NULL if no uncle
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{}
