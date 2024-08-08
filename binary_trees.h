cd3388d5321b40a22ffe237d773d35e2fd4928ae
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-08 14:12:06 -0500

    101.1 - used GPT. Just crusing/learning

--- binary_trees.h
+++ binary_trees.h
@@ -52,5 +52,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /* ADVANCED */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_levelorder_helper(const binary_tree_t *tree, int level, void (*func)(int));
 
 #endif
