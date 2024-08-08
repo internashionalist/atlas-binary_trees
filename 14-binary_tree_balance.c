718f8dce71b2a704c805690cc2a2d31a1e2c0392
internashionalist <taylor.thames@atlasschool.com>
2024-08-08 04:01:34 -0500

    14.1 - uses task 9 - somethings wrong though

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
 
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
@@ -8,4 +9,15 @@
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
-{}
+{
+	int Lheight; /* height of left subtree */
+	int Rheight; /* height of right subtree */
+
+	if (!tree) /* if no tree */
+		return (0); /* no forest? */
+
+	Lheight = binary_tree_height(tree->left); /* measure left subtree */
+	Rheight = binary_tree_height(tree->right); /* measure right subtree */
+
+	return (Lheight - Rheight); /* return balance factor */
+}
