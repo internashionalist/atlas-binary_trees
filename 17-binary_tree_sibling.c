f3806eb73f61d57672f83359a249be91dc6adca5
“internashionalist” <taylor.thames@atlaschool.com>
2024-08-08 06:10:37 -0500

    17.0 - base

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node whose sibling we're finding
+ *
+ * Return: pointer to the sibling node, or NULL if no sibling
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
