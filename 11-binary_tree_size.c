db45c615f9ea3733402bcfaeb7f4c33dcbd08e40
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 22:13:59 -0500

    11.0 - base

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures size of a binary tree
+ * @tree: pointer to the root node of measured tree
+ *
+ * Return: size of tree, or 0 if tree is NULL
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
