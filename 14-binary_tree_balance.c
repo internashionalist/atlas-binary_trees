b68379e953b9e3be765342c6456c3cddcf779fd3
internashionalist <taylor.thames@atlasschool.com>
2024-08-08 02:56:28 -0500

    14.0 - base

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of measured tree
+ *
+ * Return: balance factor of the tree, or 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{}
